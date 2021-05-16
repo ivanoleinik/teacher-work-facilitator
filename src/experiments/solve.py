d884f94b23254b7a753e05dadf27dbcddc6384c0
Ivan Oleinik <olivva239@gmail.com>
2021-05-16 21:23:48 +0300

    Add problem check function

---
+++ src/experiments/solve.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sympy
+
+LABELS_TO_SYMBOLS = {
+    0: '0', 1: '1', 2: '2', 3: '3', 4: '4',
+    5: '5', 6: '6', 7: '7', 8: '8', 9: '9',
+    10: '/', 11: '==', 12: '-', 13: '+', 14: '*'
+}
+
+
+def solve(list_of_labels):
+    try:
+        return eval(''.join(map(lambda label: LABELS_TO_SYMBOLS[label], list_of_labels)))
+    except SyntaxError:
+        return False
+
+
+if __name__ == '__main__':
+    print(solve([1, 11, 1]))
