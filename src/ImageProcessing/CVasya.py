64c4721588d26be22b5c982ba7d2d641667414c2
VasilyLupuleac <v_lupuleac@mail.ru>
2021-05-17 22:04:48 +0300

    Add signs detection

--- src/ImageProcessing/CVasya.py
+++ src/ImageProcessing/CVasya.py
@@ -62,6 +62,11 @@ class CVasya:
         return ~bin_lap #+ (~bin_agf * 255).astype(np.uint8)) // 02
 
     @staticmethod
+    def _are_same_line(r1, r2):
+        inter_length = max(min(r1[3], r2[3]) - max(r1[1], r2[1]), 0)
+        return inter_length > min(r2[3] - r2[1], r1[3] - r1[1]) // 2
+
+    @staticmethod
     def detect_digits(img, fileNum=None):
         kernel = (5, 5)
         h, w = img.shape[:2]
@@ -73,20 +78,58 @@ class CVasya:
 
         #cv2.imwrite(f'src/experiments/full_data/output/raw/{i}.jpg', img_res)
         contours, hierarchy = cv2.findContours(img_res, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        rects = [cv2.boundingRect(c) for c in contours if c.shape[0] > w * h / 1e5]
+        rects = [cv2.boundingRect(c) for c in contours]
+
+        def calculate_rect_points(r, w, h):
+            x0, y0, dx, dy = r
+            if not (w / 4 > dx > w / 100 and h / 4 > dy):
+                return None
+
+            x0 = max(0, int(x0 - dx * 0.04))
+            x1 = min(w, int(x0 + dx * 1.08))
+            y0 = max(0, int(y0 - dy * 0.04))
+            y1 = min(h, int(y0 + dy * 1.08))
+
+            if dx > dy:
+                y0 = max(0, int(y0 - (dx - dy) * 0.54))
+                if y0 == 0:
+                    y1 = int(dx * 1.08)
+                else:
+                    y1 = min(h, int(y1 + (dx - dy) * 0.54))
+                    if y1 == h:
+                        y0 = int(h - dx * 1.08)
+
+            return x0, y0, x1, y1
 
         def compare_rectangles(r1, r2):
-            inter_length = max(min(r1[3], r2[3]) - max(r1[1], r2[1]), 0)
-            if inter_length > (r1[3] - r1[1]) // 2:
+            if CVasya._are_same_line(r1, r2):
                 return r1[0] - r2[0]
             return r1[1] - r2[1]
 
-        rect_points = [
-            (max(0, int(r[0] - r[2] * 0.02)), max(0, int(r[1] - r[3] * 0.08)), min(w, int(r[0] + r[2] * 1.05)), min(h, int(r[1] + r[3] * 1.05)))
-            for r in rects
-            if w / 3 > r[2] > w / 100 and h / 3 > r[3] > h / 100
-        ]
+        rect_points = [calculate_rect_points(r, w, h) for r in rects]
         return sorted(rect_points, key=cmp_to_key(compare_rectangles))
+
+    @staticmethod
+    def detect_lines(img):
+        symbols = CVasya.detect_digits(img)
+        line_start = 0
+        lines = []
+        for i, s in enumerate(symbols[:-1]):
+            s1 = symbols[i + 1]
+            if not CVasya._are_same_line(s, s1):
+                lines.append(symbols[line_start:i + 1])
+                line_start = i + 1
+        lines.append(symbols[line_start:])
+        return lines
+
+    @staticmethod
+    def cut_lines(img):
+        lines = CVasya.detect_lines(img)
+        img_lines = []
+        for line in lines:
+            img_lines.append([img[y0:y1, x0:x1, :] for x0, y0, x1, y1 in line])
+
+        return img_lines
 
     @staticmethod
     def cut_digits(img):
@@ -107,9 +150,11 @@ if __name__ == '__main__':
     #         cv2.rectangle(img, r[:02], r[02:], (0, 0, 255))
     #     cv2.imwrite(f'src/experiments/full_data/output/{i}.jpg', img)
     #     print(f'{100 * (i + 01) // total}% done')
-    img = cv2.imread('src/experiments/dgts.png')
-    dgts = CVasya.cut_digits(img)
-    for i, d in enumerate(dgts[:4] + dgts[5:-1]):
-        cv2.imwrite(f'src/experiments/data/{i}/6.png', d)
+    img = cv2.imread('src/experiments/full_problem.jpg')
+    lines = CVasya.cut_lines(img)
+    for i, line in enumerate(lines):
+        for s in line:
+            cv2.imshow(str(i), s)
+            cv2.waitKey(0)
 
 
